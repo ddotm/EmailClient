fb66c56753566dfc3bea8088e1a73b71dffc33f4
Dmitri Mogilevski <dmogilevski@gmail.com>
2020-05-07 09:55:00 -0500

    MailgunMessage documentation.

--- DdotM.EmailClient.Mailgun/MailgunMessage.cs
+++ DdotM.EmailClient.Mailgun/MailgunMessage.cs
@@ -5,18 +5,39 @@ using System.Collections.Generic;
 
 namespace DdotM.EmailClient.Mailgun
 {
+    /// <summary>
+    /// Mailgun email message
+    /// </summary>
     public class MailgunMessage : EmailMessage
     {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public MailgunMessage()
         {
             Tags = new List<string>();
         }
 
-        // Set message delivery time - format "Fri, 14 Oct 2011 23:10:10 -0000"
+        /// <summary>
+        /// Message delivery time.
+        /// Messages are not guaranteed to arrive at exactly the requested time.
+        /// Messages can be scheduled for a maximum of 3 days in the future.
+        /// </summary>
         public DateTime? DeliveryTime { get; set; }
-        public List<string> Tags { get; set; }
+        
+        /// <summary>
+        /// Mailgun message tags
+        /// </summary>
+        public List<string> Tags { get; }
+        
+        /// <summary>
+        /// Controls message tracking. Default is false.
+        /// </summary>
         public bool Tracking { get; set; } = false;
 
+        /// <summary>
+        /// Once the message send is attempted, Response will contain the Mailgun HTTP response data
+        /// </summary>
         public IRestResponse Response { get; set; }
     }
 }
